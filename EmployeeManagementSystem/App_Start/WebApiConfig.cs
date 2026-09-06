4cf6c9d122389b742a250f441cc7595bf5a61e80
25Samiksha <khubikarsam@gmail.com>
2026-09-06 10:35:04 +0530

    updated file

--- EmployeeManagementSystem/App_Start/WebApiConfig.cs
+++ EmployeeManagementSystem/App_Start/WebApiConfig.cs
@@ -1,21 +1,16 @@
-﻿using System.Web.Http;
+using System.Web.Http;
 
-namespace EmployeeManagementSystem
+public static class WebApiConfig
 {
-    public static class WebApiConfig
+    public static void Register(HttpConfiguration config)
     {
-        public static void Register(HttpConfiguration config)
-        {
-            config.MapHttpAttributeRoutes();
-
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new
-                {
-                    id = RouteParameter.Optional
-                }
-            );
-        }
+        config.Routes.MapHttpRoute(
+            name: "DefaultApi",
+            routeTemplate: "api/{controller}/{id}",
+            defaults: new
+            {
+                id = RouteParameter.Optional
+            }
+        );
     }
 }
