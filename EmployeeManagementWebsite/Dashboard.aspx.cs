3a11f1e10e66b8f0a841541bd65e2e8a7c6a53fd
25Samiksha <khubikarsam@gmail.com>
2026-09-09 16:02:16 +0530

    Updated File

--- EmployeeManagementWebsite/Dashboard.aspx.cs
+++ EmployeeManagementWebsite/Dashboard.aspx.cs
@@ -1,14 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
 
 public partial class Dashboard : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        if (!IsPostBack)
+        {
+            BindDashboard();
+        }
+    }
 
+    private string GetConnectionString()
+    {
+        return ConfigurationManager
+            .ConnectionStrings["EmployeeDB"]
+            .ConnectionString;
+    }
+
+    private void BindDashboard()
+    {
+        using (SqlConnection con =
+            new SqlConnection(GetConnectionString()))
+        {
+            con.Open();
+            using (SqlCommand cmd =
+                new SqlCommand(
+                    "SELECT COUNT(*) FROM Employees",
+                    con))
+            {
+                lblTotalEmployees.Text =
+                    cmd.ExecuteScalar().ToString();
+            }
+            using (SqlCommand cmd =
+                new SqlCommand(
+                    "SELECT COUNT(*) FROM Employees WHERE Status = 'Active'",
+                    con))
+            {
+                lblActiveEmployees.Text =
+                    cmd.ExecuteScalar().ToString();
+            }
+            using (SqlCommand cmd =
+                new SqlCommand(
+                    "SELECT COUNT(*) FROM Employees WHERE Status = 'Inactive'",
+                    con))
+            {
+                lblInactiveEmployees.Text =
+                    cmd.ExecuteScalar().ToString();
+            }
+            using (SqlCommand cmd =
+                new SqlCommand(
+                    "SELECT COUNT(DISTINCT Department) FROM Employees",
+                    con))
+            {
+                lblDepartments.Text =
+                    cmd.ExecuteScalar().ToString();
+            }
+        }
     }
 }
