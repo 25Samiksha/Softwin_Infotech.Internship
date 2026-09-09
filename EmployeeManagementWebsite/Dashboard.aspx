0b1621a72266e8253dfed57dab0850819cad5eec
25Samiksha <khubikarsam@gmail.com>
2026-09-09 15:29:57 +0530

    Updated Employee Management Website

---
+++ EmployeeManagementWebsite/Dashboard.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Title="Dashboard" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Dashboard.aspx.cs" Inherits="Dashboard" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+<div class="container">
+
+<div class="page-header">
+<h2>Dashboard</h2>
+<ol class="breadcrumb">
+<li class="active">Home</li>
+</ol>
+</div>
+
+<div class="row">
+
+<div class="col-md-3 col-sm-6">
+<div class="panel panel-primary">
+<div class="panel-heading">Total Employees</div>
+<div class="panel-body text-center">
+<h2><asp:Label ID="lblTotalEmployees" runat="server" Text="0"></asp:Label></h2>
+</div>
+</div>
+</div>
+
+<div class="col-md-3 col-sm-6">
+<div class="panel panel-success">
+<div class="panel-heading">Active Employees</div>
+<div class="panel-body text-center">
+<h2><asp:Label ID="lblActiveEmployees" runat="server" Text="0"></asp:Label></h2>
+</div>
+</div>
+</div>
+
+<div class="col-md-3 col-sm-6">
+<div class="panel panel-danger">
+<div class="panel-heading">Inactive Employees</div>
+<div class="panel-body text-center">
+<h2><asp:Label ID="lblInactiveEmployees" runat="server" Text="0"></asp:Label></h2>
+</div>
+</div>
+</div>
+
+<div class="col-md-3 col-sm-6">
+<div class="panel panel-info">
+<div class="panel-heading">Departments</div>
+<div class="panel-body text-center">
+<h2><asp:Label ID="lblDepartments" runat="server" Text="0"></asp:Label></h2>
+</div>
+</div>
+</div>
+
+</div>
+
+</div>
+
+</asp:Content>
---
+++ EmployeeManagementWebsite/Dashboard.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+public partial class Dashboard : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            BindDashboard();
+        }
+    }
+
+    private string GetConnectionString()
+    {
+        return ConfigurationManager
+            .ConnectionStrings["EmployeeDB"]
+            .ConnectionString;
+    }
+
+    private void BindDashboard()
+    {
+        using (SqlConnection con =
+            new SqlConnection(GetConnectionString()))
+        {
+            con.Open();
+            using (SqlCommand cmd =
+                new SqlCommand(
+                    "SELECT COUNT(*) FROM Employees",
+                    con))
+            {
+                lblTotalEmployees.Text =
+                    cmd.ExecuteScalar().ToString();
+            }
+            using (SqlCommand cmd =
+                new SqlCommand(
+                    "SELECT COUNT(*) FROM Employees WHERE Status = 'Active'",
+                    con))
+            {
+                lblActiveEmployees.Text =
+                    cmd.ExecuteScalar().ToString();
+            }
+            using (SqlCommand cmd =
+                new SqlCommand(
+                    "SELECT COUNT(*) FROM Employees WHERE Status = 'Inactive'",
+                    con))
+            {
+                lblInactiveEmployees.Text =
+                    cmd.ExecuteScalar().ToString();
+            }
+            using (SqlCommand cmd =
+                new SqlCommand(
+                    "SELECT COUNT(DISTINCT Department) FROM Employees",
+                    con))
+            {
+                lblDepartments.Text =
+                    cmd.ExecuteScalar().ToString();
+            }
+        }
+    }
+}
